09580049ded3591235aaf19f8c4b262ad35f50be
Brian Schorn <schorn.brian@gmail.com>
2017-01-16 02:12:31 -0600

    Added navBar to the top with first box highlighted

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,9 +1,37 @@
+var navCounter = 0;
+
 $(document).ready(function(){
+
+  //AJAX call
     $.ajax({
       type: "GET",
       url: "/data",
       success: function(data){
         console.log(data);
+        var dataArr = data;
+        var idCounter = 0;
+        var $studentBox;
+        dataArr.forEach(function(student){
+          if(idCounter == 0){
+            $studentBox = '<div class = "box selected" id=' + idCounter + '></div>';
+          } else{
+            $studentBox = '<div class = "box" id=' + idCounter + '></div>';
+          }
+          idCounter++;
+          $('#students').append($studentBox);
+
+
+        });
+
       }
     });
+
+
+
+  //Button Listenders
+
+
+
+
+
 });
